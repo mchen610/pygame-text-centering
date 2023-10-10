2ba99529591237be7d2b13e77bf76f11efc2c45c
superlion3 <melvinchen610@gmail.com>
2023-10-10 17:33:38 -0400

    edited example.py formatting

---
+++ Upload-Library.ps1
@@ -0,0 +1,29 @@
+param(
+    [string]$m = "updated using POSH"
+)
+
+Get-ChildItem -Path ".\dist" -Filter "*.tar.gz" | Remove-Item
+
+$content = Get-Content -Path .\setup.py
+
+$content | Where-Object { $_ -match "version='(\d+\.\d+)'" }
+$currentVersion = $matches[1]
+
+$versionParts = $currentVersion -split "\." #splits version into two 
+$newMinorVersion = [int]$versionParts[1] + 1
+$newVersion = "$($versionParts[0]).$newMinorVersion"
+
+$newContent = $content -replace "version='$currentVersion'", "version='$newVersion'"
+
+$newContent | Set-Content -Path .\setup.py
+
+Invoke-Expression "python setup.py sdist"
+Invoke-Expression "git add ."   
+Invoke-Expression "git commit -m $m"
+Invoke-Expression "git push"
+Invoke-Expression "twine upload dist/*"
+
+Set-Location '..'
+
+
+# Execute setup.py
