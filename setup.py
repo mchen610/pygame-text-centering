354a09f77dc237e2076924070fed152f49f72419
mchen610 <melvinchen610@gmail.com>
2023-10-21 11:21:02 -0400

    updated to sqlite3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pygame-text-centering',
-    version='0.15',
+    version='0.16',
     packages=find_packages(),
     install_requires=[
         'pygame', 'numpy', 'joblib', 'scipy', 'threadpoolctl', 'scikit-learn'
